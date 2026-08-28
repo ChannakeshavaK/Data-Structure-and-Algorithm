b33b9220e66193e4001f009f7f861328ce5c5dfb
ChannakeshavaK <channakeshavashetty33@gmail.com>
2026-08-28 11:48:37 +0530

    Time: 3 ms (86.07%), Space: 43.3 MB (41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public boolean isValid(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray())
+        {
+            if(ch =='(' || ch=='{'||ch=='[')
+            {
+                stack.push(ch);
+            }
+            else if(ch ==')' && !stack.empty() && stack.peek() =='(' )
+            {
+                stack.pop();
+            }
+            else if(ch =='}' && !stack.empty() && stack.peek() =='{' )
+            {
+                stack.pop();
+            }
+            else if(ch ==']' && !stack.empty() && stack.peek() =='[' )
+            {
+                stack.pop();
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
