85b743013886b0d1b6b3d45766e2e64adba3fc64
ChannakeshavaK <channakeshavashetty33@gmail.com>
2026-09-03 14:57:32 +0530

    Time: 0 ms (100%), Space: 46.7 MB (79.19%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        int oldColor = image[sr][sc];
+
+        // If old and new colors are same, nothing to do
+        if (oldColor == color) {
+            return image;
+        }
+
+        dfs(image, sr, sc, oldColor, color);
+
+        return image;
+    }
+
+    private void dfs(int[][] image, int r, int c, int oldColor, int color) {
+
+        // Out of bounds
+        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length) {
+            return;
+        }
+
+        // Only fill pixels having the original color
+        if (image[r][c] != oldColor) {
+            return;
+        }
+
+        // Change color
+        image[r][c] = color;
+
+        // Up
+        dfs(image, r - 1, c, oldColor, color);
+
+        // Down
+        dfs(image, r + 1, c, oldColor, color);
+
+        // Left
+        dfs(image, r, c - 1, oldColor, color);
+
+        // Right
+        dfs(image, r, c + 1, oldColor, color);
+    }
+}
